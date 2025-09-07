c39cafc4931697a7fd826a50039533ecddf9b7c3
SH42913 <shoot913@gmail.com>
2025-09-07 22:50:42 +0400

    Remove vanilla recipes

---
+++ kubejs/server_scripts/remove_vanilla_recipes.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(function (event) {
+    event.remove({type: "minecraft:smelting"})
+    event.remove({type: "minecraft:blasting"})
+    event.remove({type: "minecraft:smoking"})
+
+    event.remove({id: "minecraft:coarse_dirt"})
+    event.remove({id: "minecraft:shears"})
+
+    // event.remove({
+    //     mod: "minecraft",
+    //     type: "minecraft:crafting_shaped"
+    // })
+
+})
