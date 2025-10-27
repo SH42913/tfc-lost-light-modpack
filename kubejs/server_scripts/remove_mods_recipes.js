db5f3cb780face54b9210ac1a6805f038aac7c44
SH42913 <shoot913@gmail.com>
2025-10-27 20:56:29 +0400

    Vanilla gems in recipes replaced with TFC gems

--- kubejs/server_scripts/remove_mods_recipes.js
+++ kubejs/server_scripts/remove_mods_recipes.js
@@ -1,3 +1,6 @@
 ServerEvents.recipes(event => {
     event.remove({output: /farmersdelight:.*knife.*/});
+
+    event.remove({id: "tfcloot:crafting/emerald_polished"})
+    event.remove({id: "tfcloot:crafting/diamond_polished"})
 })
--- kubejs/server_scripts/replace_items_in_recipes.js
+++ kubejs/server_scripts/replace_items_in_recipes.js
@@ -1,7 +1,13 @@
 ServerEvents.recipes(event => {
-    event.replaceInput(
-        {},
-        "minecraft:barrel",
-        "#tfcbarrels:barrels"
-    )
+    event.replaceInput({}, "minecraft:barrel", "#tfcbarrels:barrels")
+
+    event.replaceInput({}, "minecraft:emerald", "tfc:gem/emerald")
+    event.replaceInput({}, "minecraft:lapis_lazuli", "tfc:gem/lapis_lazuli")
+    event.replaceInput({}, "minecraft:diamond", "tfc:gem/diamond")
+    event.replaceInput({}, "minecraft:amethyst_shard", "tfc:gem/amethyst")
+
+    event.replaceOutput({}, "minecraft:emerald", "tfc:gem/emerald")
+    event.replaceOutput({}, "minecraft:lapis_lazuli", "tfc:gem/lapis_lazuli")
+    event.replaceOutput({}, "minecraft:diamond", "tfc:gem/diamond")
+    event.replaceOutput({}, "minecraft:amethyst_shard", "tfc:gem/amethyst")
 })
