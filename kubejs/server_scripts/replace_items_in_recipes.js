387992bb55a31a8b3a3c6f1ad4f0653142ae4eaa
SH42913 <shoot913@gmail.com>
2025-12-14 19:41:53 +0400

    minecraft:bowl replaced with #tfc:bowls in recipes

--- kubejs/server_scripts/replace_items_in_recipes.js
+++ kubejs/server_scripts/replace_items_in_recipes.js
@@ -15,6 +15,7 @@ ServerEvents.recipes(event => {
     event.replaceInput({}, "farmersdelight:straw", "#forge:straw")
     event.replaceInput({}, "magistuarmory:leather_strip", "#lost_light:leather_strips")
     event.replaceInput({}, "sns:leather_strip", "#lost_light:leather_strips")
+    event.replaceInput({}, "minecraft:bowl", "#tfc:bowls")
 
     event.replaceOutput({}, "minecraft:emerald", "tfc:gem/emerald")
     event.replaceOutput({}, "minecraft:diamond", "tfc:gem/diamond")
